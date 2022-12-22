77d403e9b59ec9ad4210c5e8f8e9e951ac2afbec
Ye99-AungKhant <yeaungkhant077@gmail.com>
2022-12-22 22:09:00 +0630

    props method

--- src/App.js
+++ src/App.js
@@ -1,47 +1,90 @@
 import React from 'react'
 
-class Items extends React.Component{
+class Item extends React.Component {
   render() {
     return (
-       <li>
-        {this.props.name}, ${this.props.price}
-       </li>
+       <div>
+        <li>
+          {this.props.name}, ${this.props.price}
+        </li>
+       </div>
+    );
+  }
+}
+
+class AddForm extends React.Component {
+  nameRef = React.createRef()
+  priceRef = React.createRef()
+
+add = () => {
+  let name = this.nameRef.current.value
+  let price = this.priceRef.current.value
+  this.props.add(name, price)
+}
+
+  render() {
+    return (
+       <div>
+        <input type="text" ref={this.nameRef}/>
+        <input type="text" ref={this.priceRef}/>
+        <button onClick={this.add}>ADD</button>
+       </div>
+    );
+  }
+}
+
+class Title extends React.Component {
+  render() {
+    return (
+       <div>
+        <h1>{this.props.title}</h1>
+       </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+       <div>
+        <Title title={this.props.name}/>
+       </div>
     );
   }
 }
 
 class App extends React.Component {
- state = {
-  items:[
-    
-  ]
- }
 
- add = () => {
-  let id = this.state.items.length
-  if(id === 0){
-    id +=1
-  }else{
-    id = this.state.items.length + 1
+  state = {
+    items:[
+      {id:1, name:"Apple", price:1}
+    ]
   }
 
-  this.setState({
-    items:[
-      ...this.state.items,
-      {id, name:`Fruid ${id}`, price:0.1 * id}
-    ]
-  })
- }
+  add = (name, price) => {
+    let id = this.state.items.length +1
+
+    this.setState({
+      items:[
+        ...this.state.items,
+        {id, name, price}
+      ]
+    })
+  }
 
   render() {
     return (
        <div>
-        <h1>Hello React</h1>
-        <button onClick={this.add}>ADD</button>
+        <Header name="My App"/>
+        <AddForm add={this.add}/>
         <ul>
           {this.state.items.map(i => {
             return(
-              <Items name={i.name} price={i.price}></Items>
+              <Item 
+                key={i.id}
+                name={i.name}
+                price={i.price}
+              />
             )
           })}
         </ul>
