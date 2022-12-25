f664e974d69797be213ab13211573de2c95ef145
Ye99-AungKhant <yeaungkhant077@gmail.com>
2022-12-25 18:06:22 +0630

    context hook

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,28 +1,43 @@
 import React from 'react'
 
-const MyContext = React.createContext();
+// const MyContext = React.createContext();
+
+// const MyApp = props => {
+//     return (
+//         <div>
+//             <MyContext.Provider value="Hello Context">
+//                 <Navbar/>
+//             </MyContext.Provider>
+//         </div>
+//     )
+// }
+
+// const Navbar = props => {
+//     return(
+//         <Brand/>
+//     )
+// }
+// const Brand =props => {
+//     return(
+//         <MyContext.Consumer>
+//            {value => <h1>{value}</h1>}
+//         </MyContext.Consumer>
+//     )
+// }
+
+const MyContext = React.createContext("Hello React Context")
 
 const MyApp = props => {
-    return (
-        <div>
-            <MyContext.Provider value="Hello Context">
-                <Navbar/>
-            </MyContext.Provider>
-        </div>
-    )
+    return <Navbar/>
 }
 
 const Navbar = props => {
-    return(
-        <Brand/>
-    )
+    return <Brand/>
 }
-const Brand =props => {
-    return(
-        <MyContext.Consumer>
-           {value => <h1>{value}</h1>}
-        </MyContext.Consumer>
-    )
+
+const Brand = props => {
+    let value = React.useContext(MyContext)
+    return <h1>{value}</h1>
 }
 
 export default MyApp
